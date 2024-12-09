499dce3e5b04f42b8a07118a743da4755250d37d
Ni0duann <2014567981@qq.com>
2024-12-09 21:52:41 +0800

    feat：add easy concurrent mode

--- micro-react/render.js
+++ micro-react/render.js
@@ -10,11 +10,34 @@ function render(element, container) {
     .filter((key) => key !== "children")
     .forEach((key) => (dom[key] = element.props[key]));
 
-  //递归渲染子元素
-  element.props.children.forEach((child) => render(child, dom));
+  // //递归渲染子元素
+  // element.props.children.forEach((child) => render(child, dom));
 
   //追加到父节点
   container.append(dom);
 }
+
+let nextUnitOfWork = null;
+
+//调度函数
+function workLoop(deadLine) {
+  // 应该退出
+  let shoulYield = false;
+  // 执行条件
+  while(nextUnitOfWork && !shoulYield){
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork)
+    // 检查时间
+    shoulYield = deadLine.timeRemaining() < 1;
+
+  }
+  // 没有时间就请求下一次
+  requestIdleCallback(workLoop)
+}
+
+// 第一次请求
+requestIdleCallback(workLoop)
+
+function performUnitOfWork(work){}
+
 
 export default render;
